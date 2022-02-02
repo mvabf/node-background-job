4f0879ae84d5a9f09b4d0fd99601cdf7bc6edac2
Marcus Baltazar <mvabf_dev@outlook.com>
2022-02-01 21:06:00 -0300

    bull and redis added queue implemetation started

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -8,13 +8,6 @@ export default {
             name, email, password
         };
 
-        await Mail.sendMail({
-            from: 'Queue Test <queue@test.com.br>',
-            to: `${name} <${email}>`,
-            subject: 'Cadastro de usuário',
-            html: `Olá ${name}, bem vindo ao sistema!`
-        });
-
         res.json(user);
     }
 };
